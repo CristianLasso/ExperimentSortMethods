3529fe39687fa4b7b7bd263b83ceff8c0913fc87
CristianLasso <47828389+CristianLasso@users.noreply.github.com>
2020-02-08 23:30:56 -0500

    More sort methods implemented and the main initialize, call and print the array

--- Experiment/Program.cs
+++ Experiment/Program.cs
@@ -7,6 +7,29 @@ namespace Experiment
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            int n = arr.Length;
+
+            SortingMethods sm = new SortingMethods();
+            //sm.BubbleSort(arr);
+            //sm.SelectionSort(arr);
+            //sm.QuickSort(arr, 0, arr.Length-1);
+            //sm.MergeSort(arr, 0, arr.Length-1);
+            //sm.HeapSort(arr);
+
+
+
+            Console.WriteLine("Sorted array is");
+            printArray(arr);
+        }
+
+        /* A utility function to print array of size n */
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.Read();
         }
     }
 }
